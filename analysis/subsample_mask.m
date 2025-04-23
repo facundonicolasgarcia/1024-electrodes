8d6eba00faa565c6a282d3af69d352e9631eac0e
facundo <facundogarcia2015@gmail.com>
2025-04-23 11:24:25 -0300

    Agregué cosas a subsample analysis. Corregí subsample_mask. Creé el board_plot, que es para generar la imagen del subsample. Creé mean_by_group, que genera el promedio de una variable dada otra variable.

--- analysis/subsample_mask.m
+++ analysis/subsample_mask.m
@@ -1,8 +1,8 @@
 function mask = subsample_mask(f)
     % returns mask on electrode indexes: contains ones for the subsample
     % electrodes and zeros elsewhere
-    i = mod(1:1024, 8); % row of the array of electrodes
-    j = floor(mod(1:1024, 64)/ 8)+1; % column
+    i = mod(0:1023, 8); % row of the array of electrodes
+    j = floor(mod(0:1023, 64)/ 8); % column
     
     switch f
         case 1
@@ -10,15 +10,15 @@ function mask = subsample_mask(f)
         case 2
             mask = (  mod(i, 2) == mod(j, 2)  ) ;
         case 4
-            mask = ( ( mod(i, 2) == 1 ) & mod(j, 2) == 1 );
+            mask = ( ( mod(i, 2) == 0 ) & mod(j, 2) == 0 );
         case 8
-            mask = ( mod(i, 4)==mod(j, 4) & mod(i, 2)==1);
+            mask = ( mod(i, 4)==mod(j, 4) & mod(i, 2)==0);
         case 16
-            mask = ( mod(i, 4) == mod(j, 4) & mod(1, 4) == 1);
+            mask = ( mod(i, 4) == mod(j, 4) & mod(i, 4) == 0);
         case 32
-            mask = ( i==j & (i==1 | i==5));
+            mask = ( i==j & (i==0 | i==4));
         case 64
-            mask = (i==j & i==1);
+            mask = (i==j & i==0);
     end
 end
 
